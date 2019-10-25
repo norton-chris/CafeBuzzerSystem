108710b2cef3fb551b92ea00e332758f3612643d
theNewton17 <Newton17.LuxCraft@gmail.com>
2019-10-25 13:22:53 -0400

    updating phone number parsing to fix errors

--- demo/src/Phone.java
+++ demo/src/Phone.java
@@ -1,18 +1,17 @@
 public class Phone {
 
     private int parsePhoneNumber(String input) {
-//        String chunks[] = input.split("[ ,./-]+");
-//        String out = "";
-//        foreach(String c: chunks) {
-//            out += c;
-//        }
-//        return Integer.parseInt(out);
-        //hello from erin
-        return 0;
+        String chunks[] = input.split("[ ,./-]+");
+        String out = "";
+        for(String c: chunks) {
+            out += c;
+        }
+        System.out.println(out);
+        return Integer.parseInt(out);
     }
 
-    public static void main(int args[]) {
-
-        //System.out.println(parsePhoneNumber("715-944-9124"));
+    public static void main(String args[]) {
+        Phone phone = new Phone();
+        System.out.println(phone.parsePhoneNumber("715-944-9124"));
     }
 }
