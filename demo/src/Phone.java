7478580739d96bbce7df2a0ed79bfd9ae03cbd6d
theNewton17 <Newton17.LuxCraft@gmail.com>
2019-10-25 13:38:50 -0400

    updated phone number parsing function

--- demo/src/Phone.java
+++ demo/src/Phone.java
@@ -1,17 +1,18 @@
 public class Phone {
 
-    private int parsePhoneNumber(String input) {
-        String chunks[] = input.split("[ ,./-]+");
+    private String parsePhoneNumber(String input) {
+        String chunks[] = input.split("[ ,./)(-]+");
         String out = "";
         for(String c: chunks) {
             out += c;
         }
-        System.out.println(out);
-        return Integer.parseInt(out);
+        return out;
     }
 
     public static void main(String args[]) {
         Phone phone = new Phone();
         System.out.println(phone.parsePhoneNumber("715-944-9124"));
+        System.out.println(phone.parsePhoneNumber("(715)944-9124"));
+        System.out.println(phone.parsePhoneNumber("715 944 9124"));
     }
 }
