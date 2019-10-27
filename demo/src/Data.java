00249e0c04c15448d48e162d8aaa16bc46675ecf
Chris <chris.norton132@gmail.com>
2019-10-27 18:04:12 -0400

    Created data class
    created Data class to store order numbers, phone numbers, and emails. Added two hashmaps for phone and email, but this might change

---
+++ demo/src/Data.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Data {
+
+    HashMap<Integer, Integer> orderPhone = new HashMap<>(); // This hashmap holds all the order number and phone number pairs
+    HashMap<Integer, Integer> orderemail = new HashMap<>(); // This hashmap holds all the order number and email pairs
+
+    
+
+
+}
