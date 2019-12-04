eb1bd9aa57314d76e66dc95807a852ad060108cb
Anthony Safford <adsaffor@mtu.edu>
2019-12-04 14:01:30 -0500

    Front End Rehaul
    Controller class and main java app have been modified, also FXML sheet has been added

---
+++ demo/src/Controller.java
@@ -0,0 +1,100 @@
+import com.sun.mail.imap.protocol.MessageSet;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static javafx.application.Application.launch;
+
+public class Controller
+{
+
+    Email email = new Email();
+    Phone phone = new Phone();
+    MessageBox msgBox = new MessageBox();
+    OrderIOManager io = new OrderIOManager();
+    private userUI ui;
+    @FXML
+    private TextField orderNum;
+
+    @FXML
+    private TextField phoneNum;
+
+    @FXML
+    private TextField emailAdd;
+
+    @FXML
+    private Button send;
+
+    public Controller(){};
+    private void initalize(){};
+
+    public void setApp(userUI ui){
+        this.ui = ui;
+    }
+    public void handleSubmitButton(ActionEvent event){
+        System.out.println("Send Button Pressed!");
+        try {
+            if(!orderNum.getText().isEmpty())
+            {
+                int orderNumPars = Integer.parseInt(orderNum.getText());
+                String orderNumRecieve = orderNum.getText();
+                if(!emailAdd.getText().isEmpty()) {
+                    String emailReceive = emailAdd.getText();
+                    email.sendInitial(emailReceive);
+                    msgBox.putMessage(orderNumPars, emailReceive, "");
+                    emailAdd.clear();
+                }
+                if(!phoneNum.getText().isEmpty()){
+                    String phoneRecieve = phoneNum.getText();
+                    phone.sendInitial(phoneRecieve);
+                    msgBox.putMessage(orderNumPars, phoneRecieve, "");
+                    phoneNum.clear();
+                }
+                orderNum.clear();
+            }
+            else{
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText("PLEASE ENTER YOUR ORDER NUMBER!");
+            }
+            //            else if(!phoneNum.getText().isEmpty() && emailAdd.getText().isEmpty()){
+//                String phoneReceive = phoneNum.getText();
+//                int orderNumPars = Integer.parseInt(orderNum.getText());
+//                //phone.sendInitial(phoneReceive);
+//                msgBox.putMessage(orderNumPars, "", phoneReceive);
+//                phoneNum.clear();
+//
+//            }
+//            else if(!phoneNum.getText().isEmpty() && !emailAdd.getText().isEmpty()){
+//                String emailReceive = emailAdd.getText();
+//                int orderNumPars = Integer.parseInt(orderNum.getText());
+//                String phoneReceive = phoneNum.getText();
+//                email.sendInitial(emailReceive);
+//                msgBox.putMessage(orderNumPars, emailReceive, phoneReceive);
+//                emailAdd.clear();
+//            }
+            orderNum.clear();
+
+
+//            io.writeHashMap();
+        } catch (Exception e) {
+            System.out.println("PROBLEM IN retrieve EMAIL!");
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[]args)
+    {
+
+    }
+}
--- demo/src/UserUI.java
+++ demo/src/UserUI.java
@@ -1,146 +1,56 @@
-import com.sun.javafx.geom.Rectangle;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.geometry.Rectangle2D;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.*;
 
+public class userUI extends Application {
+    private Stage stage;
+    private BorderPane rootLayout;
 
-import javax.mail.MessagingException;
-
-import static javafx.application.Application.launch;
-
-public class UserUI extends Application
-{
     @Override
-    public void start(Stage stage)
-    {
-        MessageBox msgBox = new MessageBox();
-        Stage stage2 = new Stage();
-        int status = 0;
+    public void start(Stage stage){
+        this.stage = stage;
+        this.stage.setTitle("Cafe Notification System");
 
-        int HEIGHT = 600;
-        int WIDTH = 1000;
+        layout();
+//        showController();
+    }
 
-        Email email = new Email();
-        Phone phone = new Phone();
+    public void layout(){
+        try{
+            FXMLLoader load = new FXMLLoader();
+            load.setLocation(getClass().getResource("userUIsheet.fxml"));
+            rootLayout =  (BorderPane) load.load();
 
-        VBox vbox = new VBox();
-        Pane pane = new Pane();
-        vbox.setAlignment(Pos.CENTER);
-        vbox.setSpacing(20);
-        stage.setTitle("Cafe Notification System");
-        stage2.setTitle("Sending Notification!");
-
-        TextField phoneNumTxt = new TextField();
-        TextField emailTxt = new TextField();
-        TextField orderNumTxt = new TextField();
-        Button send = new Button("Send");
-        Label emailLabel = new Label("Email: ");
-        Label phoneLabel = new Label("Phone Number: ");
-        Label orderNum = new Label("Order Number: ");
-
-        phoneNumTxt.setMaxSize(600, 100);
-        orderNumTxt.setMaxSize(600, 100);
-        emailTxt.setMaxSize(600, 100);
-
-
-
-
-
-        //stage 2 construction
-        ImageView loadingGif = new ImageView();
-        loadingGif.setImage(new Image("loadingCircles.gif"));
-        loadingGif.toBack();
-        loadingGif.setLayoutX(45);
-        Text loading = new Text("Sending your notification!");
-        loading.setLayoutX(105);
-        loading.setLayoutY(300);
-        loading.setTextAlignment(TextAlignment.CENTER);
-        loading.setFont(new Font("Helvetica", 35));
-
-        send.setDefaultButton(true); // set so that enter will press the send button
-        send.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Send Button Pressed!");
-                try {
-                    if(!emailTxt.getText().isEmpty()) {
-                        String emailReceive = emailTxt.getText();
-                        int orderNumPars = Integer.parseInt(orderNumTxt.getText());
-                        stage.hide();
-                        stage2.show();
-                        email.sendInitial(emailReceive);
-                        msgBox.putMessage(orderNumPars, emailReceive, "");
-                        emailTxt.clear();
-                    }
-                    if(!phoneNumTxt.getText().isEmpty()){
-                        String phoneReceive = phoneNumTxt.getText();
-                        int orderNumPars = Integer.parseInt(orderNumTxt.getText());
-                        stage.hide();
-                        stage2.show();
-                        phone.sendInitial(phoneReceive);
-                        msgBox.putMessage(orderNumPars, "", phoneReceive);
-                        phoneNumTxt.clear();
-
-                    }
-                    orderNumTxt.clear();
-
-                    if(email.getStatus()==1)
-                    {
-                        stage2.hide();
-                        stage.show();
-                    }
-                    else{
-                        System.out.println("ERROR IN SENDING EMAIL CAUGHT IN UI");
-                    }
-
-                    if(phone.getStatus()==1)
-                    {
-                        stage2.hide();
-                        stage.show();
-                    }
-
-                } catch (Exception e) {
-                    System.out.println("PROBLEM IN retrieve EMAIL!");
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        vbox.getChildren().addAll(emailLabel, emailTxt, phoneLabel, phoneNumTxt, orderNum, orderNumTxt, send);
-        pane.getChildren().addAll(loading, loadingGif);
-
-        // Set the Properties of the Stage
-        stage.setX(100);
-        stage.setY(200);
-        stage.setMinHeight(300);
-        stage.setMinWidth(400);
-
-        Scene scene = new Scene(vbox, WIDTH, HEIGHT);
-        Scene scene2 = new Scene(pane, 600, 400, Color.BLACK);
-        stage.setScene(scene);
-        stage2.setScene(scene2);
-        stage.show();
-
+            Scene scene = new Scene(rootLayout);
+            stage.setScene(scene);
+            stage.show();
         }
-    public static void main(String[]args)
-    {
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void showController() {
+        try{
+        FXMLLoader load = new FXMLLoader();
+        load.setLocation(getClass().getResource("userUIsheet.fxml"));
+        AnchorPane overview = (AnchorPane) load.load();
+        rootLayout.setCenter(overview);
+
+
+        Controller controller = load.getController();
+        controller.setApp(this);
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[]args){
         launch(args);
     }
 }
